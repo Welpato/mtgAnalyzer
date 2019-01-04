1a519b71b5c0b5786b93ba9633e21899d35e0407
Wesley Osorio <wesleyfosorio@hotmail.com>
2019-01-04 01:08:30 +0100

    Chaging the regex process so it can be used for any type of column

--- src/entity/regexEntity.py
+++ src/entity/regexEntity.py
@@ -1,16 +1,4 @@
-class colorPie:
-	class color:
-		def __init__(self, colorName, colorShort):
-			self.colorName = colorName
-			self.colorShort = colorShort
-
-	white = color('White','W')
-	blue = color('Blue', 'U')
-	black = color('Black', 'B')
-	red = color('Red', 'R')
-	green = color('Green', 'G')
-	colorList = [ white, blue, black, red, green]
-
+class regex:
 	#Return a regex that checks if the colors informed exists in the column
 	#TODO:Check how to do a "AND" on the regex in place o the "OR" = "|"
 	def regexExistsColor(self, colorsToCheck, capsuleOpen = "'", capsuleClose = "'"):
