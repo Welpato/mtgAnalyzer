7195b5f6875e1a2f25f4127ece0bb93e666f78c1
Wesley Osorio <wesleyfosorio@hotmail.com>
2019-02-04 18:50:04 +0100

    Adding more info to the draft analisys

--- src/entity/colorPieEntity.py
+++ src/entity/colorPieEntity.py
@@ -9,4 +9,6 @@ class colorPie:
 	black = color('Black', 'B')
 	red = color('Red', 'R')
 	green = color('Green', 'G')
-	colorList = [ white, blue, black, red, green]
+	colorListObject = [ white, blue, black, red, green]
+	colorListName = [ white.colorName, blue.colorName, black.colorName, red.colorName, green.colorName]
+	colorListShort = [ white.colorShort, blue.colorShort, black.colorShort, red.colorShort, green.colorShort]
